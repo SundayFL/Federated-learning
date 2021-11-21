9d747301c74f433801cdd2cbf300f8ffdf1202c0
Z-Xbeova <mwpk310@gmail.com>
2021-11-21 23:17:40 +0100

    extension

--- akka-server/Server/src/main/java/example/akka/remote/server/DelaySupervisor.java
+++ akka-server/Server/src/main/java/example/akka/remote/server/DelaySupervisor.java
@@ -3,8 +3,12 @@ package example.akka.remote.server;
 import akka.actor.*;
 
 public class DelaySupervisor extends UntypedActor {
-
     public DelaySupervisor(){
+
+    }
+
+    @Override
+    public void onReceive(Object o) throws Exception {
 
     }
 }
